56bb6b19b1490724e9d156c8a402fbcda278b915
fangtaoysu <18394604239@163.com>
2024-03-29 15:01:59 +0800

    add all

--- base/test.cpp
+++ base/test.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
+const int a = 1, b = 1;
 int main() {
-    cout << "hello" << endl;
+    
+    string c, d;
+    static int e, f;
+    cout << &(a) << endl;
+    cout << &(b) << endl;
+    cout << &(c) << endl;
+    cout << &(d) << endl;
+    cout << &("dfas") << endl;
+    cout << &(e) << endl;
+
     return 0;
 }
